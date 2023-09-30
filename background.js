b45f719f17a5c72db74edcd94d84e61e8afc451d
Pramod <pramod7@gmail.com>
2023-09-30 21:37:53 +0530

    separated checking for bookmarks and pasted links

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.action.onClicked.addListener((tab) => {
     chrome.sidePanel.setOptions({tabId: tab.id, enabled: true});
   });
 
-// Allow users to open the sidebar by clicking on the action toolbar icon
+// Open the sidebar on clicking on the action toolbar icon
 chrome.sidePanel
 .setPanelBehavior({ openPanelOnActionClick: true })
 .catch((error) => console.error(error));
